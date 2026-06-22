43f0153271c81ee52fb524558863a08d80f179ce
Your Name <ganaagreg@gmail.com>
2026-06-22 21:49:25 +0800

    feat: configure Google Fonts with cyrillic-ext subsets

---
+++ app/fonts.ts
@@ -0,0 +1,22 @@
+import { Cormorant_Garamond, Golos_Text, Space_Mono } from 'next/font/google'
+
+export const cormorant = Cormorant_Garamond({
+  subsets: ['latin', 'cyrillic-ext'],
+  weight: ['300', '400'],
+  variable: '--font-display',
+  display: 'swap',
+})
+
+export const golos = Golos_Text({
+  subsets: ['latin', 'cyrillic-ext'],
+  weight: ['400', '500'],
+  variable: '--font-body',
+  display: 'swap',
+})
+
+export const spaceMono = Space_Mono({
+  subsets: ['latin'],
+  weight: ['400'],
+  variable: '--font-utility',
+  display: 'swap',
+})
